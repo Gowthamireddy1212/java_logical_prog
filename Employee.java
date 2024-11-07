df0e4e5d5115ce701ad392b1d671997b918ac5be
Gowthamireddy1212 <gowthamireddyeruva@gmail.com>
2024-11-07 14:47:51 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+class Employee
+{
+public static void main(String[]args)
+{
+int eno=22;
+String ename="Gowthami";
+float ft=10000F;
+
+System.out.println(eno);
+System.out.println("The eno value is:"+eno);
+System.out.print("The Given ename is:"+ename);
+System.out.println("The Given Decimal value is:"+ft);
+}
+}
